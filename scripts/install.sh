8d8cbeadd2d96fbf41659dba8e0fe69b5e895409
Yang Liu <i@yangliu.name>
2021-02-06 11:15:57 +0800

    remove git from install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,9 @@
 #!/bin/ash
 acg_path="/opt/acg"
 
-git --version 1>/dev/null 2>/dev/null
-if [ $? != 0 ]; then
-  apk add --update git
-fi
-
-git clone -b main https://github.com/yangliu/alpine-clash-gateway.git "${acg_path}"
+wget -O /tmp/acg.zip https://github.com/yangliu/alpine-clash-gateway/archive/main.zip
+unzip /tmp/acg.zip -d /tmp
+mv /tmp/alpine-clash-gateway-main "${acg_path}"
 chmod +x "${acg_path}/scripts/acg.sh"
 ${acg_path}/scripts/acg.sh install
 
