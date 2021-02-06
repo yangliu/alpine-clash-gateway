77b2b6c47f69fbe12c007c5b4977111ac72f69dc
Yang Liu <i@yangliu.name>
2021-02-06 14:40:49 +0800

    allow install to somewhere other than /opt

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
-acg_path="/opt/acg"
+
+if [ -z ${1} ]; then
+  acg_path="/opt/acg"
+else
+  acg_path="${1}"
+fi
+
+[ ! -d "${acg_path}" ] && mkdir -p "${acg_path}"
 
 wget -O /tmp/acg.zip https://github.com/yangliu/alpine-clash-gateway/archive/main.zip
 unzip /tmp/acg.zip -d /tmp
-mv /tmp/alpine-clash-gateway-main "${acg_path}"
+cp -R /tmp/alpine-clash-gateway-main/* "${acg_path}/"
+escaped_acg_path=$(printf '%s\n' "${acg_path}" | sed -e 's/[]\/$*.^[]/\\&/g');
+sed -i "s/acg_path=\"\/opt\/acg\"/acg_path=\"${escaped_acg_path}\"" "${acg_path}/files/acg"
+sed -i "s/acg_path=\"\/opt\/acg\"/acg_path=\"${escaped_acg_path}\"" "${acg_path}/scripts/*.sh"
 chmod +x "${acg_path}/scripts/acg.sh"
 ${acg_path}/scripts/acg.sh install
 
